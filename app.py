c3ad8880346521d7e6081d29dc99667a7037e04c
Vyom_Dwivedi <83266292+vyomdwivedi@users.noreply.github.com>
2024-09-16 11:41:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,35 +3,35 @@ import shutil
 import py7zr
 import streamlit as st
 from io import BytesIO
+import tempfile
+import os
 
 def compressor(uploaded_file, ff_name):
     start_time = time.time()
-    new_file_name = f"{ff_name}.rar"
-    with py7zr.SevenZipFile(new_file_name, 'w') as archive:
-        archive.write(uploaded_file, ff_name)
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".rar") as temp_file:
+        new_file_name = temp_file.name
+        with py7zr.SevenZipFile(new_file_name, 'w') as archive:
+            archive.write(ff_name, uploaded_file.name)
     end_time = time.time()
     st.write(f'Time taken: {end_time - start_time} seconds')
-    st.write("Compression complete.")
+    st.success("Compression complete.")
     return new_file_name
 
 def decompressor(uploaded_file, extract_name):
     start_time = time.time()
-    fn_without_extension = extract_name[:-len(".rar")]
-    dir = "/tmp"  # Temporary directory for extraction
+    temp_dir = tempfile.mkdtemp()
     try:
         with py7zr.SevenZipFile(uploaded_file, 'r') as archive:
-            archive.extractall(dir)
+            archive.extractall(temp_dir)
     except Exception as e:
         st.error(f"Error extracting RAR file: {e}")
         return None
     end_time = time.time()
     st.write(f'Time taken: {end_time - start_time} seconds')
-    st.write("Extraction complete.")
-    return dir
+    st.success("Extraction complete.")
+    return temp_dir
 
 def main():
-    # st.subheader(f"Welcome {st.session_state.email}")
-
     function = st.selectbox("What do you want to do?", ("Compress", "Decompress"))
 
     if function == "Compress":
@@ -53,10 +53,12 @@ def main():
             if uploaded_file and extract_name:
                 extracted_dir = decompressor(uploaded_file, extract_name)
                 if extracted_dir:
-                    shutil.make_archive(extracted_dir, 'zip', extracted_dir)
-                    with open(f"{extracted_dir}.zip", "rb") as file:
+                    zip_file_path = shutil.make_archive(extracted_dir, 'zip', extracted_dir)
+                    with open(zip_file_path, "rb") as file:
                         st.download_button(label="Download Extracted Files", data=file, file_name=f"{extract_name}.zip")
+                    shutil.rmtree(extracted_dir)  # Clean up the temporary directory
             else:
                 st.error("Please upload a RAR file and enter the RAR name.")
+
 if __name__ == "__main__":
     main()
